0b759378aaa123c4e266694ced74e412f4a5058e
RItesh <shuklaritesh5678@gmail.com>
2024-08-31 13:49:20 +0530

    Customized the backend and database to fetch data based on the certain filter and return them

--- src/routes/filterRoute.js
+++ src/routes/filterRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { allAuthors, allGenres, allBooks } from "../controllers/FilterController.js";
+import { allAuthors, allGenres, allBooks,filteredBooks } from "../controllers/FilterController.js";
 
 const filterRouter = Router();
 
 filterRouter.get('/all-authors',allAuthors );
 filterRouter.get('/all-genres',allGenres)
 filterRouter.get('/all-books', allBooks)
+filterRouter.post('/custom-filter', filteredBooks);
 
 export default filterRouter;
